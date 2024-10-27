b471df403f7d5ad94de8176403d0343b30c4289c
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-10-27 21:19:06 +0700

    video 18

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "./components/layout/app.header";
 
 function Layout() {
 
   return (
     <div>
-      hello world with hoidanit
+      <AppHeader />
+      <Outlet />
     </div>
   )
 }
