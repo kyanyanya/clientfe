1868275f1e35d05f207a11d10cf5b0ed73152ddd
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-11-01 22:03:52 +0700

    video 45

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -1,9 +1,9 @@
 import { getUsersAPI } from '@/services/api';
-import { PlusOutlined } from '@ant-design/icons';
+import { DeleteTwoTone, EditTwoTone, PlusOutlined } from '@ant-design/icons';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
-import { ProTable, TableDropdown } from '@ant-design/pro-components';
-import { Button, Space, Tag } from 'antd';
-import { useRef } from 'react';
+import { ProTable } from '@ant-design/pro-components';
+import { Button } from 'antd';
+import { useRef, useState } from 'react';
 
 
 const columns: ProColumns<IUserTable>[] = [
@@ -13,8 +13,14 @@ const columns: ProColumns<IUserTable>[] = [
         width: 48,
     },
     {
-        title: '_id',
+        title: 'Id',
         dataIndex: '_id',
+        hideInSearch: true,
+        render(dom, entity, index, action, schema) {
+            return (
+                <a href='#'>{entity._id}</a>
+            )
+        },
     },
     {
         title: 'Full Name',
@@ -23,31 +29,43 @@ const columns: ProColumns<IUserTable>[] = [
     {
         title: 'Email',
         dataIndex: 'email',
+        copyable: true
     },
     {
         title: 'Created At',
         dataIndex: 'createdAt',
     },
+
     {
-        title: 'hoidanit',
-        dataIndex: 'title',
-        copyable: true,
-        ellipsis: true,
-        tooltip: '标题过长会自动收缩',
-        formItemProps: {
-            rules: [
-                {
-                    required: true,
-                    message: '此项为必填项',
-                },
-            ],
-        },
-    },
+        title: 'Action',
+        hideInSearch: true,
+        render(dom, entity, index, action, schema) {
+            return (
+                <>
+                    <EditTwoTone
+                        twoToneColor="#f57800"
+                        style={{ cursor: "pointer", marginRight: 15 }}
+                    />
+                    <DeleteTwoTone
+                        twoToneColor="#ff4d4f"
+                        style={{ cursor: "pointer" }}
+                    />
+                </>
+
+            )
+        }
+    }
 
 ];
 
 const TableUser = () => {
     const actionRef = useRef<ActionType>();
+    const [meta, setMeta] = useState({
+        current: 1,
+        pageSize: 5,
+        pages: 0,
+        total: 0
+    });
     return (
         <>
             <ProTable<IUserTable>
@@ -57,20 +75,28 @@ const TableUser = () => {
                 request={async (params, sort, filter) => {
                     console.log(sort, filter);
                     const res = await getUsersAPI();
-
+                    if (res.data) {
+                        setMeta(res.data.meta);
+                    }
                     return {
                         data: res.data?.result,
-                        "page": 1,
-                        "success": true,
-                        "total": res.data?.meta.total
+                        page: 1,
+                        success: true,
+                        total: res.data?.meta.total
                     }
 
                 }}
-                rowKey="id"
-                pagination={{
-                    pageSize: 5,
-                    onChange: (page) => console.log(page),
-                }}
+                rowKey="_id"
+                pagination={
+                    {
+                        current: meta.current,
+                        pageSize: meta.pageSize,
+                        showSizeChanger: true,
+                        total: meta.total,
+                        showTotal: (total, range) => { return (<div> {range[0]}-{range[1]} trên {total} rows</div>) }
+                    }
+                }
+
                 headerTitle="Table user"
                 toolBarRender={() => [
                     <Button
