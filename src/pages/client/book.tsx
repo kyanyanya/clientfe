cc87ee55c3e973baa523fa16fe15d13d338f5eef
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-11-17 15:02:27 +0700

    video 72

--- src/pages/client/book.tsx
+++ src/pages/client/book.tsx
@@ -1,8 +1,19 @@
+import BookDetail from "@/components/client/book/book.detail";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const BookPage = () => {
+    let { id } = useParams();
+
+    useEffect(() => {
+        if (id) {
+            //do something
+            console.log("book id = ", id)
+        }
+    }, [id])
     return (
         <div>
-            BookPage
+            <BookDetail />
         </div>
     )
 }
