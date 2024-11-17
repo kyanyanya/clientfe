021614f4be76e3f725e4b30acb96777a7f47dd4b
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-11-17 16:09:49 +0700

    video  74

--- src/pages/client/book.tsx
+++ src/pages/client/book.tsx
@@ -1,4 +1,5 @@
 import BookDetail from "@/components/client/book/book.detail";
+import BookLoader from "@/components/client/book/book.loader";
 import { getBookByIdAPI } from "@/services/api";
 import { App } from "antd";
 import { useEffect, useState } from "react";
@@ -8,10 +9,12 @@ const BookPage = () => {
     let { id } = useParams();
     const { notification } = App.useApp();
     const [currentBook, setCurrentBook] = useState<IBookTable | null>(null);
+    const [isLoadingBook, setIsLoadingBook] = useState<boolean>(true);
 
     useEffect(() => {
         if (id) {
             const fetchBookById = async () => {
+                setIsLoadingBook(true);
                 const res = await getBookByIdAPI(id);
                 if (res && res.data) {
                     setCurrentBook(res.data);
@@ -21,15 +24,20 @@ const BookPage = () => {
                         description: res.message
                     })
                 }
+                setIsLoadingBook(false);
             }
             fetchBookById();
         }
     }, [id])
     return (
         <div>
-            <BookDetail
-                currentBook={currentBook}
-            />
+            {isLoadingBook ?
+                <BookLoader />
+                :
+                <BookDetail
+                    currentBook={currentBook}
+                />
+            }
         </div>
     )
 }
