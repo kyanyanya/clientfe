9556982986d8f3d21bd8f076e12448d7c6d7c6b6
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-11-19 21:19:36 +0700

    video 81

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,8 @@ import ManageBookPage from 'pages/admin/manage.book';
 import ManageOrderPage from 'pages/admin/manage.order';
 import ManageUserPage from 'pages/admin/manage.user';
 import LayoutAdmin from 'components/layout/layout.admin';
+import OrderPage from 'pages/client/order';
+
 import enUS from 'antd/locale/en_US';
 import viVN from 'antd/locale/vi_VN';
 
@@ -34,6 +36,10 @@ const router = createBrowserRouter([
       {
         path: "/book/:id",
         element: <BookPage />,
+      },
+      {
+        path: "/order",
+        element: <OrderPage />
       },
       {
         path: "/about",
---
+++ src/pages/client/order.tsx
@@ -0,0 +1,11 @@
+import OrderDetail from "@/components/client/order"
+
+const OrderPage = () => {
+    return (
+        <>
+            <OrderDetail />
+        </>
+    )
+}
+
+export default OrderPage;
